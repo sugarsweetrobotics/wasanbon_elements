2ee22824f7194d7b00683c4859b89ab161538e1c
Yuki Suga <ysuga@ysuga.net>
2016-02-10 23:16:51 +0900

    add wasanbon element

--- lib/wasanbon_toolbar.dart
+++ lib/wasanbon_toolbar.dart
@@ -3,16 +3,34 @@
 library wasanbon_toolbar;
 
 import 'dart:html' as html;
+import 'dart:async' as async;
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:polymer/polymer.dart';
+
+typedef EventFunction(a, b);
+
 
 @PolymerRegister('wasanbon-toolbar')
 class WasanbonToolbar extends PolymerElement {
 
+  @property
+  EventFunction onBack;
+
   WasanbonToolbar.created() : super.created();
+
+//  async.StreamController<dynamic> onback;
+
+  void attached() {
+//    onback = new async.Stream.empty();
+    onBack = () {};
+  }
+
+  void goBack() {
+    html.window.location.assign('http://${Uri.base.host}:${Uri.base.port}');
+  }
 
   @reflectable
   void onTap(var e, var d) {
-    html.window.location.assign('http://${Uri.base.host}:${Uri.base.port}');
+    onBack(e, d);
   }
 }
